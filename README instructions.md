746b0b16e269746535a2400e5266a6d5ceeb1b40
Abhishek Choudhari <abhishekc97@gmail.com>
2023-11-01 22:17:12 +0530

    Added elastic client for indexing, creating index in /create-book api, deleting index in /delete api and updating in /update api

---
+++ README instructions.md
@@ -0,0 +1,11 @@
+Books application
+
+Please clone the repository, and cd into it
+Start the app using: node app.js
+The mongo DB and elastic search cluster is hosted online.
+
+Make a .env and/or .env.local and paste variables from the google docs page
+(modify port if needed)
+
+HOST=127.0.0.1
+PORT=3010
--- routes/books.js
+++ routes/books.js
@@ -3,8 +3,18 @@ const route = Router();
 
 const Book = require("../models/Book");
 
+// Elasticsearch client
+const { Client } = require("@elastic/elasticsearch");
+const elasticClient = new Client({
+    node: process.env.ELASTIC_CLUSTER_ENDPOINT,
+    auth: {
+        username: process.env.ELASTIC_USERNAME,
+        password: process.env.ELASTIC_PASSWORD,
+    },
+});
+
 /**
- * POST requet to create a new book
+ * POST request to create a new book
  */
 route.post("/create-book", async function (req, res, next) {
     try {
@@ -25,7 +35,25 @@ route.post("/create-book", async function (req, res, next) {
             description: description,
         };
 
+        // create the book in mongodb collection
         const result = await Book.create(newBook);
+
+        // elastic search index step
+        const { statusCode, body } = await elasticClient.index({
+            index: "books", // Index name
+            id: result._id, // _id from mongo result
+            body: newBook, // newBook object
+        });
+        console.log(body);
+        console.log(statusCode);
+        if (statusCode === 201) {
+            console.log(
+                "Document indexed in elastic cloud successfully:",
+                body
+            );
+        } else {
+            console.log("Unexpected status code:", statusCode);
+        }
 
         if (!result) {
             return res.status(400).send("Could not create a new book entry.");
@@ -44,6 +72,7 @@ route.post("/create-book", async function (req, res, next) {
  */
 route.get("/all", async function (req, res, next) {
     try {
+        // find all the books in mongo collection and return
         const results = await Book.find({});
 
         if (!results) {
@@ -72,6 +101,7 @@ route.get("/:id", async function (req, res, next) {
                 );
         }
 
+        // find the book by id in mongo collection and return
         const result = await Book.findById(bookId);
 
         if (!result) {
@@ -99,7 +129,7 @@ route.put("/update/:id", async function (req, res, next) {
                     "Bad request, check given parameters. Please include book id."
                 );
         }
-
+        // get the request body parameters
         const { title, author, publication, isbn, description } = req.body;
 
         const updatedDetails = {
@@ -110,11 +140,27 @@ route.put("/update/:id", async function (req, res, next) {
             description: description,
         };
 
+        // find the book by id and update it, then return updated book in response
         const updatedBook = await Book.findByIdAndUpdate(
             bookId,
             { $set: updatedDetails },
             { new: true } // mongo returns the updated document
         );
+
+        // Update the book in Elasticsearch
+        const { statusCode, body } = await elasticClient.update({
+            index: "books", // Elasticsearch index name
+            id: bookId, // Use the book's ID for Elasticsearch
+            body: {
+                doc: updatedDetails, // Updated book data
+            },
+        });
+
+        if (statusCode === 200) {
+            console.log("Elasticsearch: Document updated successfully");
+        } else {
+            console.log("Elasticsearch: Unexpected status code:", statusCode);
+        }
 
         return res.status(200).json(updatedBook);
     } catch (error) {
@@ -137,7 +183,20 @@ route.delete("/delete/:id", async function (req, res, next) {
                 );
         }
 
+        // find the book by id and delete
         const deletedBook = await Book.findByIdAndRemove(bookId);
+
+        // Delete the book from Elasticsearch
+        const { statusCode, body } = await elasticClient.delete({
+            index: "books", // Elasticsearch index name
+            id: bookId, // Use the book's ID for Elasticsearch
+        });
+
+        if (statusCode === 200) {
+            console.log("Elasticsearch: Document deleted successfully");
+        } else {
+            console.log("Elasticsearch: Unexpected status code:", statusCode);
+        }
 
         if (!deletedBook) {
             return res.status(404).json("Book not found");
@@ -149,6 +208,49 @@ route.delete("/delete/:id", async function (req, res, next) {
     } catch (error) {
         console.log(error);
         next(error);
+    }
+});
+
+/**
+ * SEARCH API
+ */
+route.get("/search/query", async function (req, res, next) {
+    try {
+        const { query } = req.query;
+
+        console.log(query);
+        if (!query) {
+            return res
+                .status(400)
+                .json({ error: "Query parameter is required" });
+        }
+
+        // Elasticsearch search api
+        const { body } = await elasticClient.search({
+            index: "books",
+            body: {
+                query: {
+                    multi_match: {
+                        query: query,
+                        fields: ["title", "author", "description"],
+                        operator: "and",
+                    },
+                },
+            },
+        });
+        console.log(body);
+        console.log(JSON.stringify(body, null, 2));
+
+        if (body && body.hits) {
+            const searchResults = body.hits.hits.map((hit) => hit._source);
+            console.log(searchResults);
+            return res.json(searchResults);
+        } else {
+            return res.status(500).json({ error: "No search results found" });
+        }
+    } catch (error) {
+        next(error);
+        console.log(error);
     }
 });
 
